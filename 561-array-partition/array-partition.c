97d40b9f375759ec39bcf8538807ef31b966ebec
saranes2023-byte <saranes.2023@vitstudent.ac.in>
2026-01-31 18:57:59 +0530

    Time: 20 ms (67.31%) | Memory: 10.9 MB (23.27%) - LeetSync

---
+++ 561-array-partition/array-partition.c
@@ -0,0 +1,13 @@
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int arrayPairSum(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);
+
+    int sum = 0;
+    for (int i = 0; i < numsSize; i += 2)
+        sum += nums[i];
+
+    return sum;
+}
